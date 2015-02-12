2aca2a34fcd40f5cced729c33fc62c8518e6d1df
Joe Woodhouse <joewoodhouse@outlook.com>
2015-02-12 18:06:21 +0000

    Updated service and servicerunner classes. Reduce dependencies

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var ConfigUtils = require('./lib/configUtils');
 var Service = require('./lib/service');
+var ScriptRunner = require('./lib/scriptRunner');
+
 var FlowXOSDK = module.exports = {
   ConfigUtils: ConfigUtils,
-  Service: Service
+  Service: Service,
+  ScriptRunner: ScriptRunner
 };
--- tasks/run.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = function(grunt){
-  grunt.registerTask('run', 'Run a service method', function(method){
-  });
-};
