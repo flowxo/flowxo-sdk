bd3936adf90df4ac613130f44c12f122f6538724
Joe Woodhouse <joewoodhouse@outlook.com>
2015-02-17 16:04:06 +0000

    Import utils from flowxo core

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var ConfigUtils = require('./lib/configUtils');
 var Service = require('./lib/service');
 var ScriptRunner = require('./lib/scriptRunner');
+var Utils = require('./lib/utils');
 
 var FlowXOSDK = module.exports = {
   ConfigUtils: ConfigUtils,
   Service: Service,
-  ScriptRunner: ScriptRunner
+  ScriptRunner: ScriptRunner,
+  Utils: Utils
 };
