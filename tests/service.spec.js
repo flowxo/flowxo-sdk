43611d3731b7778e874c44c075ac0515a9345e24
Joe Woodhouse <joewoodhouse@outlook.com>
2015-04-07 23:18:03 +0100

    Add simple method to get a method config by name

--- tests/service.spec.js
+++ tests/service.spec.js
@@ -35,26 +35,26 @@ describe('FlowXO SDK Service',function(){
   describe('#new',function(){
     it('should create a service with the correct values',function(){
       var json = service.toJSON();
-      json.should.be.a('object');
-      json.name.should.equal('testname');
-      json.slug.should.equal('testslug');
-      json.methods.should.be.a('array');
+      expect(json).to.be.an('object');
+      expect(json.name).to.equal('testname');
+      expect(json.slug).to.equal('testslug');
+      expect(json.methods).to.be.an('array');
     });
   });
 
   describe('#runMethodScript',function(){
     it('should throw error when method is not defined',function(done){
       service.runMethodScript('notamethod','run',{},function(err,result){
-        err.should.exist();
-        should.not.exist(result);
+        expect(err).to.exist;
+        expect(result).to.not.exist;
         done();
       });
     });
 
     it('should throw an error when a script of a method is not defined',function(done){
       service.runMethodScript('add_test','notascript',{},function(err,result){
-        err.should.exist();
-        should.not.exist(result);
+        expect(err).to.exist;
+        expect(result).to.not.exist;
         done();
       });
     });
@@ -62,10 +62,10 @@ describe('FlowXO SDK Service',function(){
     it('should run a method successfully',function(done){
       service.runMethodScript('add_test','run',{test:1},function(err,result){
         // The method should have echoed back the input
-        should.not.exist(err);
-        result.should.exist();
-        result.should.be.a('object');
-        result.test.should.equal(1);
+        expect(err).to.not.exist;
+        expect(result).to.exist;
+        expect(result).to.be.an('object');
+        expect(result).to.have.property('test',1);
         done();
       });
     });
@@ -74,17 +74,17 @@ describe('FlowXO SDK Service',function(){
   describe('#runServiceScript',function(){
     it('should throw error when script is not defined',function(done){
       service.runServiceScript('notascript',{},function(err,result){
-        err.should.exist();
-        should.not.exist(result);
+        expect(err).to.exist;
+        expect(result).to.not.exist;
         done();
       });
     });
 
     it('should run a service script successfully',function(done){
       service.runServiceScript('ping',{ping: true},function(err,result){
-        should.not.exist(err);
-        result.should.exist();
-        assert.propertyVal(result,'ping',true);
+        expect(err).to.not.exist;
+        expect(result).to.exist;
+        expect(result).to.have.property('ping',true);
         done();
       });
     });
@@ -92,11 +92,12 @@ describe('FlowXO SDK Service',function(){
 
   describe('#registerMethod',function(){
     it('should add a method to the service',function(){
+      expect(service.methods.length).to.equal(1);
       service.methods.length.should.equal(1);
       service.registerMethod({
         name: 'newmethod'
       });
-      service.methods.length.should.equal(2);
+      expect(service.methods.length).to.equal(2);
     });
   });
 
@@ -108,6 +109,19 @@ describe('FlowXO SDK Service',function(){
 
   describe('#toJSON',function(){
     it('should return all required fields',function(){
+    });
+  });
+
+  describe('#getMethod',function(){
+    it('should return method config',function(){
+      var method = service.getMethod('add_test');
+      expect(method).to.exist;
+      expect(method).to.be.an('object');
+      expect(method).to.have.property('slug','add_test');
+    });
+
+    it('should return undefined if no such method',function(){
+      expect(service.getMethod('not_a_method')).to.not.exist;
     });
   });
 
